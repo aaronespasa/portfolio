d146d8d741a832de3fe918fa06024fb9bc8ba793
aaronespasa <aaespasa@gmail.com>
2021-11-12 00:33:54 +0100

    Modifying logo component

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
-// import Link from 'next/link'
-// import Image from 'next/image'
-// import { Text, useColorModeValue } from '@chakra-ui/react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
@@ -17,6 +17,23 @@ padding: 10px;
 }
 `
 
-// const Logo = () => {
-//     const footPrintImg = `/images/footprint`
-// }
+const Logo = () => {
+    const footPrintImg = '/images/logo-no-stroke.png'
+
+    return (
+        <Link href="/">
+            <a>
+                <LogoBox>
+                    <Image src={footPrintImg} width={20} height={20} alt="Logo" />
+                    <Text
+                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                        fontFamily='M PLUS Rounded 1c'
+                        fontWeight="bold"
+                        ml={3}>
+                        Aarón Espasandín
+                    </Text>
+                </LogoBox>
+            </a>
+        </Link>
+    )
+}
