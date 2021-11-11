0cc57a42830cbc5ae7e871ed5667af3523727544
aaronespasa <aaespasa@gmail.com>
2021-11-11 23:45:44 +0100

    Creating the App component and the Layout for next.js

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+/*
+ * Next.js uses the `App` component to initialize pages.
+ * I modify this component to add a Layout and the ChakraProvider to all pages.
+ */
+
+// For Chakra UI to work correctly, it's important to set up the
+// ChakraProvider at the root of the application!
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+
+const Website = ({ Component, pageProps, router }) => {
+    return (
+        <ChakraProvider>
+            {/* The Layout will add some width to all pages */}
+            <Layout router={router}>
+                {/* Component will represent all other pages */}
+                <Component {...pageProps} key={router.route} />
+            </Layout>
+        </ChakraProvider>
+    )
+}
+
+export default Website
