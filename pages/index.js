3cfac05d1d16ea8ecae97042410f6d66f609695e
aaronespasa <aaespasa@gmail.com>
2021-11-12 04:37:56 +0100

    Adding a smooth transition between pages
    This transition is defined in the file components/layouts/article.js. It has been applied as a component that covers all the contents of index and all the content of the works section.

--- pages/index.js
+++ pages/index.js
@@ -9,97 +9,100 @@ import {
     useColorModeValue
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
-        <Container>
-            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
-                Hello, I&apos;m a Computer Vision Engineer based in Spain!
-            </Box>
+        <Layout>
+            <Container>
+                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                    Hello, I&apos;m a Computer Vision Engineer based in Spain!
+                </Box>
 
-            <Box display={{md: 'flex'}}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        Aarón Espasandín
+                <Box display={{md: 'flex'}}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            Aarón Espasandín
+                        </Heading>
+                        <p>@aaronespasa ( Developer / Engineer )</p>
+                    </Box>
+
+                    <Box
+                        flexShrink={0}
+                        mt={{base: 4, md: 0}}
+                        ml={{md: 6}}
+                        align="center"
+                    >
+                        <Image
+                            borderColor="whiteAlpha.800"
+                            borderWidth={2}
+                            borderStyle="solid"
+                            maxWidth="100px"
+                            display="inline-block"
+                            borderRadius="full"
+                            src="/images/aaronespasa.jpeg"
+                            alt="Profile Image"
+                        />
+                    </Box>
+                </Box>
+
+                <Section delay={0.1}>
+                    <Heading as="h3" variant="section-title">
+                        Work
                     </Heading>
-                    <p>@aaronespasa ( Developer / Engineer )</p>
-                </Box>
+                    <Paragraph>
+                        &nbsp;&nbsp;&nbsp;&nbsp;Aaron is a computer vision engineer based in Madrid with a passion
+                        for teaching and building innovative services which can improve people&apos;s lifes.
+                        He has a knack for coming up with new impactful ideas and planning and designing them
+                        to create a usable prototype as quickly as possible (<NextLink href="https://youtu.be/b4dMker1JlE">
+                            <Link>36h challenge</Link>
+                        </NextLink>).
+                        <br />
+                        Currently, he is building his own app called PaintWall to help people decide which color to use for their walls by seeing the walls painted in real time.
+                    </Paragraph>
+                    <Box align="center" my={4}>
+                        <NextLink href="/works">
+                            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal" >
+                                My portfolio
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
 
-                <Box
-                    flexShrink={0}
-                    mt={{base: 4, md: 0}}
-                    ml={{md: 6}}
-                    align="center"
-                >
-                    <Image
-                        borderColor="whiteAlpha.800"
-                        borderWidth={2}
-                        borderStyle="solid"
-                        maxWidth="100px"
-                        display="inline-block"
-                        borderRadius="full"
-                        src="/images/aaronespasa.jpeg"
-                        alt="Profile Image"
-                    />
-                </Box>
-            </Box>
-
-            <Section delay={0.1}>
-                <Heading as="h3" variant="section-title">
-                    Work
-                </Heading>
-                <Paragraph>
-                    &nbsp;&nbsp;&nbsp;&nbsp;Aaron is a computer vision engineer based in Madrid with a passion
-                    for teaching and building innovative services which can improve people&apos;s lifes.
-                    He has a knack for coming up with new impactful ideas and planning and designing them
-                    to create a usable prototype as quickly as possible (<NextLink href="https://youtu.be/b4dMker1JlE">
-                        <Link>36h challenge</Link>
-                    </NextLink>).
-                    <br />
-                    Currently, he is building his own app called PaintWall to help people decide which color to use for their walls by seeing the walls painted in real time.
-                </Paragraph>
-                <Box align="center" my={4}>
-                    <NextLink href="/works">
-                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal" >
-                            My portfolio
-                        </Button>
-                    </NextLink>
-                </Box>
-            </Section>
-
-            <Section delay={0.2}>
-                <Heading as="h3" variant="section-title">
-                    Bio
-                </Heading>
-                <BioSection>
-                    <BioYear>2002</BioYear>
-                    Born in Asturias, Spain.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2017</BioYear>
-                    Got right into the field of Front-end development.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2018</BioYear>
-                    Made my first freelance project for a lingerie&apos;s shop.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2019</BioYear>
-                    Participated in the Merkle&apos;s Divisadero 2019 Datathon and started learning Machine Learning at the beginning of the year.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2020</BioYear>
-                    Won the Second Prize of an International Hackathon (Hack the Future Challenge), competing against workers from top tech companies.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2021 to present</BioYear>
-                    Working on two projects for improving AI education (DeepGang) and help people decide which wall color paint to buy using Augmented Reality (PaintWall).
-                </BioSection>
-            </Section>
-        </Container>
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Bio
+                    </Heading>
+                    <BioSection>
+                        <BioYear>2002</BioYear>
+                        Born in Asturias, Spain.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2017</BioYear>
+                        Got right into the field of Front-end development.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2018</BioYear>
+                        Made my first freelance project for a lingerie&apos;s shop.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2019</BioYear>
+                        Participated in the Merkle&apos;s Divisadero 2019 Datathon and started learning Machine Learning at the beginning of the year.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        Won the Second Prize of an International Hackathon (Hack the Future Challenge), competing against workers from top tech companies.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021 to present</BioYear>
+                        Working on two projects for improving AI education (DeepGang) and help people decide which wall color paint to buy using Augmented Reality (PaintWall).
+                    </BioSection>
+                </Section>
+            </Container>
+        </Layout>
     )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 import thumbLearnBox from '../public/images/learnbox-thumbnail.png'
 import thumbPaintWall from '../public/images/paintwall-thumbnail.jpeg'
 import thumbRoute from '../public/images/route-a-star-openstreetmap-thumbnail.png'
@@ -8,39 +9,41 @@ import thumbImageCaptioning from '../public/images/image-captioning-thumbnail.pn
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-            
-            {/* columns={[1, 1, 2]} = columns={{sm: 1, md: 2}} */}
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="learnbox" title="LearnBox" thumbnail={thumbLearnBox}>
-                        A video teleconferencing add-on to improve teacher&apos;s feedback by
-                        creating real time surveys just with the voice and allowing students to answering with hand gestures
-                    </WorkGridItem>
-                </Section>
+        <Layout>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Works
+                </Heading>
+                
+                {/* columns={[1, 1, 2]} = columns={{sm: 1, md: 2}} */}
+                <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                    <Section delay={0.1}>
+                        <WorkGridItem id="learnbox" title="LearnBox" thumbnail={thumbLearnBox}>
+                            A video teleconferencing add-on to improve teacher&apos;s feedback by
+                            creating real time surveys just with the voice and allowing students to answering with hand gestures
+                        </WorkGridItem>
+                    </Section>
 
-                <Section>
-                    <WorkGridItem id="paintwall" title="PaintWall" thumbnail={thumbPaintWall}>
-                        An Augmented Reality app to help people decide which color to use for their walls by seeing the walls painted in real time
-                    </WorkGridItem>
-                </Section>
+                    <Section delay={0.1}>
+                        <WorkGridItem id="paintwall" title="PaintWall" thumbnail={thumbPaintWall}>
+                            An Augmented Reality app to help people decide which color to use for their walls by seeing the walls painted in real time
+                        </WorkGridItem>
+                    </Section>
 
-                <Section>
-                    <WorkGridItem id="router-a-star" title="A Star applied to OpenStreetMap in C++" thumbnail={thumbRoute}>
-                        A program simulating Google Maps where the A Star algorithm is applied by using C++ to draw the shortest path in the OpenStreet map
-                    </WorkGridItem>
-                </Section>
+                    <Section delay={0.1}>
+                        <WorkGridItem id="router-a-star" title="A Star applied to OpenStreetMap in C++" thumbnail={thumbRoute}>
+                            A program simulating Google Maps where the A Star algorithm is applied by using C++ to draw the shortest path in the OpenStreet map
+                        </WorkGridItem>
+                    </Section>
 
-                <Section>
-                    <WorkGridItem id="image-captioning" title="Generate an automatic description of an image with Pytorch" thumbnail={thumbImageCaptioning}>
-                        A program that helps visually impaired people by generating an automatic description of what&apos;s in an image of the web.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+                    <Section delay={0.1}>
+                        <WorkGridItem id="image-captioning" title="Generate an automatic description of an image with Pytorch" thumbnail={thumbImageCaptioning}>
+                            A program that helps visually impaired people by generating an automatic description of what&apos;s in an image of the web.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
